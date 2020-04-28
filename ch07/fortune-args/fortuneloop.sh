78da10ac353e60b29f88b461b2ddabda8fb5aefc
wuYin <wuYinPost@gmail.com>
2020-04-28 09:12:04 +0800

    update

---
+++ ch07/fortune-args/fortuneloop.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+trap "exit" SIGINT
+
+INTERVAL=$1
+echo Configured to generate new fortune every $INTERVAL seconds
+
+mkdir -p /var/htdocs
+
+while :; do
+  echo $(date) Writing fortune to /var/htdocs/index.html
+  /usr/games/fortune >/var/htdocs/index.html
+  sleep $INTERVAL
+done
